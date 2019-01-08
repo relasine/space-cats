79995430a09b08c0c96399af581f3cff56682b41
Kevin Simpson <relasine@gmail.com>
2019-01-08 13:26:43 -0700

    Add catHelper object

---
+++ src/utils/catHelper.js
@@ -0,0 +1,16 @@
+export const catHelper = [
+  "./cats/cat-one.png",
+  "./cats/cat-two.png",
+  "./cats/cat-three.png",
+  "./cats/cat-four.png",
+  "./cats/cat-five.png",
+  "./cats/cat-eight.png",
+  "./cats/cat-nine.png",
+  "./cats/cat-ten.png",
+  "./cats/cat-eleven.png",
+  "./cats/cat-twelve.png",
+  "./cats/cat-thirteen.png",
+  "./cats/cat-fourteen.png",
+  "./cats/cat-fifteen.png",
+  "./cats/cat-sixteen.png"
+];
