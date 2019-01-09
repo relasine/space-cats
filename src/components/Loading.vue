f21c6ec1ee113559d47a5e6ba624aad5785cb50a
Kevin Simpson <relasine@gmail.com>
2019-01-08 21:47:45 -0700

    Add Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="loading">
+    <p class="loading-message">Wrangling cat...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.loading-message {
+  text-align: center;
+  font-family: "Verdana";
+}
+</style>
